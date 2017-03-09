f3de90f8649ba52c991c569bae11ae552f917fa9
Juan Manuel Zaragoza <juan.zaragoza@teracode.com>
2017-03-09 10:04:53 -0300

    Container MainVideoContainer -> Componente MainVideo

--- source/components/MainVideo.js
+++ source/components/MainVideo.js
@@ -1,17 +1,48 @@
 import React, { PropTypes } from "react";
+import { connect } from 'react-redux'
+
 import { MainBoxSearchBAContainer } from "../components/MainBoxSearchBA"
 
-const MainVideo = (onScrollMainVideo) => (
-    <section id="video" >
-        <video width="100%" autoPlay={true} loop="loop" preload="auto" muted >
-            <source src="content/videos/Participacion_Ciudadana_Pagina720.mp4" />
-        </video>
-        <MainBoxSearchBAContainer />
-    </section>
-)
+export class MainVideo extends React.Component {
 
-MainVideo.propTypes = {
-  onScrollMainVideo: PropTypes.func.isRequired
+	constructor(props) {
+        super(props)
+    }
+
+    render() {
+
+    	return (
+		    <section id="video" >
+		        <video width="100%" autoPlay={true} loop="loop" preload="auto" muted >
+		            <source src="content/videos/Participacion_Ciudadana_Pagina720.mp4" />
+		        </video>
+		        <MainBoxSearchBAContainer />
+		    </section>
+		)
+
+    }
+
 }
 
-export default MainVideo
+
+//container
+/*const mapStateToProps = (state, ownProps) => {
+
+    return {
+        text: state.button.active
+    }
+}*/
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onScrollMainVideo: () => {
+      //dispatch(toggleState())
+      console.log("scroll")
+    }
+  }
+}
+
+export const MainVideoContainer = connect(
+  null,
+  mapDispatchToProps
+)(MainVideo)
