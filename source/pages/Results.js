87a880aa51dcf2cca28d2dba569a9cd7af51cfb5
Juan Manuel Zaragoza <juan.zaragoza@teracode.com>
2017-03-23 11:27:59 -0300

    Buscar tambien por Tags en el buscador principal

--- source/pages/Results.js
+++ source/pages/Results.js
@@ -15,6 +15,7 @@ const Results = (props) => {
 	let sizeCols = [9,3] //primera columna de la pagina de 9/12 y la segunda de 3/12
 	var filter = {
 		nombre: props.location.query.nombre,
+		tag_nombre: props.location.query.nombre,
 		fecha: props.location.query.fecha,
 		seccion: props.location.query.seccion
 	};
