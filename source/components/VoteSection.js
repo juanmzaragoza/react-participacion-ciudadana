bc176d5972a7b41713d47a09f07bb83b2d2b792a
Juan Manuel Zaragoza <juan.zaragoza@teracode.com>
2017-04-27 16:21:12 -0300

    Mensaje de votacion exitosa

--- source/components/VoteSection.js
+++ source/components/VoteSection.js
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from 'react-redux';
+
+import { Alert, Button } from "react-bootstrap";
+
 import { VoteFormContainer } from "../components/VoteForm";
 import { VoteGraphContainer } from "../components/VoteGraph";
 
@@ -8,6 +11,9 @@ export class VoteSection extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.state ={
+			alertVisible: true
+		}
 	}
 
 	componentDidUpdate(){
@@ -16,6 +22,12 @@ export class VoteSection extends React.Component {
 			this.refs.description.innerHTML = this.props.contenido;
 		}
  	}
+
+ 	handleAlertDismiss() {
+	    this.setState({
+	    	alertVisible: false
+	    });
+	}
 
  	componentWillUnmount(){
  		if(this.refs.shortdescription !== undefined && this.refs.description !== undefined){
@@ -37,6 +49,15 @@ export class VoteSection extends React.Component {
 			 	</article>
 			 	<div className="clear"></div>
 
+			 	{(this.props.showSuccessVoteMessage && this.state.alertVisible)?
+					<Alert bsStyle="success" onDismiss={this.handleAlertDismiss.bind(this)}>
+			          <h4>Gracias por tu voto!</h4>
+			          <p>Tu opinión servirá para hacer que cada obra contribuya a moldear la ciudad que todos queremos</p>
+			        </Alert>
+					:
+					null
+				}
+
 			  	<div className="row">
 					{this.props.showGraph?
 						<VoteGraphContainer />
@@ -53,6 +74,7 @@ export class VoteSection extends React.Component {
 const mapStateToProps = (state, ownProps) => {
     return {
       showGraph: state.voteForm.graph.show,
+      showSuccessVoteMessage: state.voteForm.graph.show && state.user.isAuthenticated,
       titulo: (state.voteForm.votation.content != null)? state.voteForm.votation.content.nombre:undefined,
       descripcion_breve: (state.voteForm.votation.content != null && state.voteForm.votation.content.descripcion != null)? state.voteForm.votation.content.descripcion.slice(0, 20):null,
       contenido: (state.voteForm.votation.content != null)? state.voteForm.votation.content.descripcion:null,
